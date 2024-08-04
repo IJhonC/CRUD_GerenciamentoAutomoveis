48eb34348a48e3de8856833a58cb523959d60563
IJhonC <joaovitorcristofolini@gmail.com>
2024-08-04 17:23:12 -0300

    HotFix fechamento das telas

--- src/TelaPesquisa.java
+++ src/TelaPesquisa.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 import javax.swing.*;
@@ -27,9 +29,16 @@ public class TelaPesquisa extends JFrame {
         // Formulário de Pesquisa
         super("Pesquisa de Automóveis");
         setSize(708, 450);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
+        
+         // Adiciona um listenar para atulizar os botões caso o usuário aperta no X para fexhar a tela
+         addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e){
+                App.btnPesquisar.setEnabled(true);;
+            }
+        });
 
         // Bordas e Cores
         Border gray = BorderFactory.createLineBorder(Color.WHITE);
@@ -106,6 +115,8 @@ public class TelaPesquisa extends JFrame {
                 atualizarTabela();
             }
         });
+
+       
 
         /*
          * Adiciona o listner na tabela para quando der duplo clique, fechar a tela
